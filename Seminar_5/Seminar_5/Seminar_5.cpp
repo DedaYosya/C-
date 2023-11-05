d70541bbfed6b5214c1938926c1f0733ed43f122
Evgeniy <growery@yandex.ru>
2023-11-06 01:06:24 +0700

    seminar_5

---
+++ Seminar_5/Seminar_5/Seminar_5.cpp
@@ -0,0 +1,22 @@
+﻿// Seminar_5.cpp : Урок 5 Цикл << for >>
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    
+   
+    int i = 0;
+    for (; i <= 100; i++) {
+        
+           
+        }
+    cout << i << endl;
+    }
+    
+   
+
