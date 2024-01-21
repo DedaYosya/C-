87d62841fdfc103f7366f7354e52f77682f50d71
Evgeniy <growery@yandex.ru>
2024-01-21 21:24:22 +0700

    +job

--- 2.Основы языка С++/Lessons_6/Lessons_6.cpp
+++ 2.Основы языка С++/Lessons_6/Lessons_6.cpp
@@ -44,7 +44,7 @@ int main() {
         cin >> arr[i];
     }
     // Сортировка
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1; ++i) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
@@ -61,4 +61,5 @@ int main() {
     cout << endl;
     system("PAUSE");
     return 0;
+    
 }
--- Job/job_1/job_1.cpp
+++ Job/job_1/job_1.cpp
@@ -1,20 +1,64 @@
 ﻿// job_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+//#include <iostream>
+//#include <fstream>
+//#include <vector>
+//using namespace std;
+//int main()
+//{
+//    int i= 15;
+//    string s = "hello";
+//    double pi = 3.14;
+//    vector<int> v{ 1, 5, 7, 4, -2, 9 };
+//
+///*    cin >> i;
+//    cout << i << endl*/;
+//    ofstream fout("file.txt");
+//    if (!fout) {
+//        cout << "Error!" << endl;
+//        return -1;
+//    }
+//    fout << i << endl;
+//    fout << s << endl;
+//    fout << pi << endl;
+//    for (auto elem : v) {
+//        fout << elem << " ";
+//    }
+//    fout.close();
+//}
+
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//using namespace std;
+//int main() {
+//	ifstream fin("file.txt");
+//	string s;
+//	while (getline(fin, s)) {
+//
+//
+//		cout << s << endl;
+//	}
+//	fin.close();
+//
+//}
+
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
-int main()
-{
-    int i= 15;
+int main() {
+	ifstream fin("file.txt");
+	string s;
+	if (!fin) {
+		cout << "Error!" << endl;
+		return -1;
+	}
+	while (fin) {
+		fin >> s;
+		cout << s << endl;
+	}
+	fin.close();
 
-/*    cin >> i;
-    cout << i << endl*/;
-    ofstream fout("e:\\file.txt");
-    if (!fout) {
-        cout << "Error!" << endl;
-        return -1;
-    }
-    fout << i << endl;
 }
-
