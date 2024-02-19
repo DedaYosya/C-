b2d348d765d97e7661dc8b994af9c0b1abd19f97
Evgeniy <growery@yandex.ru>
2024-02-20 04:21:08 +0700

    +дз_3

---
+++ 3.Объектно-ориентированное программирование/Lessons_3/Lessons_3.cpp
@@ -0,0 +1,57 @@
+﻿// 1.	Создать класс Animal. 
+// Добавить в класс приватное поле возраст(int) и публичное  поле название(string) животного. 
+// Создать конструктор для инициализации этих полей. 
+// Создать три объекта класса Animal в функции main.
+// 2.	Создать метод print(), который выводит на экран название и возраст животного.
+// Вызвать этот метод для всех объектов в функции main.
+// 3.	Создать метод birthday(int number), 
+// который добавляет к возрасту животного число number и вызывает метод print
+//
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Создаем класс
+class Animal {
+    int age; // приватный
+public:
+    string name; // публичный
+
+    // Конструктор
+    Animal(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    // Метод для возраста
+    int print() {
+        return age;
+    }
+
+    // Метод добавления к возрасту
+     int birthday(int number) {
+         age += number;
+        print();
+        return age;
+    }
+};
+int main() {
+    setlocale(LC_ALL, "Rus");
+    
+    // Объявляем класс
+    Animal a1("Слон", 70);
+    Animal a2("Лев", 35);
+    Animal a3("Носорог", 78);
+    // Выводим в консоль
+    cout << "Животное: " << a1.name << " / Возраст: " << a1.print() << endl;
+    cout << "Животное: " << a2.name << " / Возраст: " << a2.print() << endl;
+    cout << "Животное: " << a3.name << " / Возраст: " << a3.print() << endl;
+    cout << "-- Возраст после добавления" << endl;
+    cout << "Животное: " << a1.name << " / Возраст: " << a1.birthday(10) << endl;
+    cout << "Животное: " << a2.name << " / Возраст: " << a2.birthday(10) << endl;
+    cout << "Животное: " << a3.name << " / Возраст: " << a3.birthday(10) << endl;
+    system("PAUSE");
+    return 0;
+}
--- Job/job_1/job_1.cpp
+++ Job/job_1/job_1.cpp
@@ -1,26 +1,2 @@
 ﻿#include <iostream>
-using namespace std;
-
-class Person {
-	int age;
-public:
-	string name;
-	double height;
-
-	Person() {
-		cout << "вызван Конструктор!" << endl;
-		name = "Иван";
-		height = 170;
-		age = 17;
-
-		cout << "Возраст: " << age << endl;
-	}
-};
-
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	Person p1;
-
-	cout << p1.name << endl;
-}
+#include <string>
